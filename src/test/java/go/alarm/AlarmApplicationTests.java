ae4a74496e616f5aa19c3651f7ff23e6cc887f30
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-08-01 14:54:20 +0900

    fix: contextLoads() 메서드 실패 에러 해결

--- src/test/java/go/alarm/AlarmApplicationTests.java
+++ src/test/java/go/alarm/AlarmApplicationTests.java
@@ -3,7 +3,7 @@ package go.alarm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AlarmApplicationTests.class)
 class AlarmApplicationTests {
 
 	@Test

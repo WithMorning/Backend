819585d31bb5134b31de521adad1f3a7581d97ea
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-08-08 14:17:21 +0900

    feat: 로그인에 필요한 Repository 메소드 추가-#3

--- src/main/java/go/alarm/domain/repository/UserRepository.java
+++ src/main/java/go/alarm/domain/repository/UserRepository.java
@@ -1,8 +1,23 @@
 package go.alarm.domain.repository;
 
 import go.alarm.domain.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findBySocialLoginId(String socialLoginId);
+
+    boolean existsByNickname(String nickname);
+
+    @Modifying
+    @Query("""
+            UPDATE Users user
+            SET user.status = 'DELETED'
+            WHERE user.id = :userId
+            """)
+    void deleteByUserId(@Param("userId") final Long userId);
 
 }

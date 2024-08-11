7379280c7b6247de831e40c3ed5824fdda98764e
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-08-11 17:43:44 +0900

    feat: 로그인 관련 dto 추가-#3
    * feat: LoginRequest 추가
    
    * feat: AccessToken을 임시 저장할 클래스 추가

---
+++ src/main/java/go/alarm/login/dto/AccessToken.java
@@ -0,0 +1,16 @@
+package go.alarm.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccessToken {
+
+    private String accessToken;
+}
--- src/main/java/go/alarm/login/dto/LoginResponse.java
+++
@@ -1,2 +0,0 @@
-package go.alarm.login.dto;public class LoginResponse {
-}

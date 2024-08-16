41e5276929f1067b9bba42f8c82d9b23b61599e0
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-08-16 17:38:21 +0900

    hotfix: OAuth 인증 에러 로그 출력

--- src/main/java/go/alarm/login/domain/OauthProviders.java
+++ src/main/java/go/alarm/login/domain/OauthProviders.java
@@ -4,12 +4,14 @@ import static go.alarm.global.response.ResponseCode.NOT_SUPPORTED_OAUTH_SERVICE;
 
 import go.alarm.global.response.exception.AuthException;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /*
 * OauthProvider를 저장하는 객체
 * */
 @Component
+@Slf4j
 public class OauthProviders {
 
     private final List<OauthProvider> providers;
@@ -19,6 +21,7 @@ public class OauthProviders {
     }
 
     public OauthProvider mapping(final String providerName) {
+        log.warn("프로바이더 이름은 ->" + providerName);
         return providers.stream()
             .filter(provider -> provider.is(providerName))
             .findFirst()

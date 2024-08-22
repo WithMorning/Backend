1c895bc23196871c46497a45a1e2f0e1b52bd48c
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-08-22 16:44:38 +0900

    refactor: accessToken 재발급 API 응ㅇ답값에 refreshToken 제거-#48

--- src/main/java/go/alarm/login/dto/LoginTokenResponse.java
+++ src/main/java/go/alarm/login/dto/LoginTokenResponse.java
@@ -1,5 +1,6 @@
 package go.alarm.login.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 public class LoginTokenResponse {
 
     private String accessToken;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL) // 어노테이션이 적용된 필드가 null인 경우, JSON 응답에서 해당 필드는 제외
     private String refreshToken;
 
     public LoginTokenResponse(String accessToken) {

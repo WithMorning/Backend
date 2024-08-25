443f59002f84cef8ae425c20abe39d53f693d895
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-08-25 18:22:59 +0900

    refactor: 메인페이지 조회 API 사용자 인증 추가-#54

--- src/main/java/go/alarm/home/presentation/HomeController.java
+++ src/main/java/go/alarm/home/presentation/HomeController.java
@@ -1,5 +1,8 @@
 package go.alarm.home.presentation;
 
+import go.alarm.auth.Auth;
+import go.alarm.auth.UesrOnly;
+import go.alarm.auth.domain.Accessor;
 import go.alarm.global.response.SuccessResponse;
 import go.alarm.home.dto.response.HomeResponse;
 import go.alarm.home.service.HomeService;
@@ -9,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,13 +25,14 @@ public class HomeController {
         private final HomeService homeService;
 
         @GetMapping("/home")
+        @UesrOnly
         @Operation(summary = "메인페이지 조회 API", description = "메인페이지를 조회합니다.")
         @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "OK, 성공입니다.")
         })
-        public SuccessResponse<HomeResponse.HomeDTO> getMainPage(@RequestHeader(name = "userId") Long userId) {
-            //소셜로그인이 들어가면 위 헤더 부분이 사라지고 토큰으로 유저를 구분해야함.
+        public SuccessResponse<HomeResponse.HomeDTO> getMainPage(
+            @Auth final Accessor accessor){
 
-            return new SuccessResponse<>(homeService.getHome(userId));
+                return new SuccessResponse<>(homeService.getHome(accessor.getUserId()));
         }
 }

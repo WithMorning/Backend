db934e64c75654426c1477c33144fe061787d680
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-08-25 20:59:11 +0900

    refactor: Group, User 도메인 관련 컨트롤러에 사용자 인증 추가-#54

--- src/main/java/go/alarm/group/presentation/GroupController.java
+++ src/main/java/go/alarm/group/presentation/GroupController.java
@@ -2,6 +2,7 @@ package go.alarm.group.presentation;
 
 
 import go.alarm.auth.Auth;
+import go.alarm.auth.UesrOnly;
 import go.alarm.auth.domain.Accessor;
 import go.alarm.group.domain.Group;
 import go.alarm.group.domain.UserGroup;
@@ -24,7 +25,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,12 +38,10 @@ public class GroupController {
     private final GroupService groupService;
 
     @PostMapping
+    @UesrOnly
     @Operation(summary = "그룹 생성(알람 생성) API", description = "그룹(알람)을 생성합니다.")
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201",description = "OK, 성공입니다.")
-    })
-    @Parameters({
-        @Parameter(name = "userId", description = "유저의 아이디, header에 담아주시면 됩니다.")
     })
     public SuccessResponse<GroupResponse> createGroup(
         @Auth final Accessor accessor,
@@ -54,49 +52,48 @@ public class GroupController {
     }
 
     @PatchMapping("/{groupId}")
+    @UesrOnly
     @Operation(summary = "그룹 수정(알람 수정) API", description = "그룹(알람)을 수정합니다.")
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201",description = "OK, 성공입니다.")
     })
-    @Parameters({
-        @Parameter(name = "userId", description = "유저의 아이디, header에 담아주시면 됩니다.")
-    })
-    public SuccessResponse<Void> updateGroup(@RequestHeader(name = "userId") Long userId,
-                                                                    @PathVariable(name = "groupId") Long groupId,
-                                                                    @RequestBody @Valid GroupRequest request) {
+    public SuccessResponse<Void> updateGroup(
+        @Auth final Accessor accessor,
+        @PathVariable(name = "groupId") Long groupId,
+        @RequestBody @Valid GroupRequest request) {
 
-        groupService.updateGroup(userId, groupId, request);
+        groupService.updateGroup(accessor.getUserId(), groupId, request);
         return new SuccessResponse<>();
     }
 
     @DeleteMapping("/{groupId}")
+    @UesrOnly
     @Operation(summary = "그룹 삭제(알람 삭제) API", description = "그룹(알람)을 삭제합니다.")
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201",description = "OK, 성공입니다.")
     })
-    @Parameters({
-        @Parameter(name = "userId", description = "유저의 아이디, header에 담아주시면 됩니다.")
-    })
-    public SuccessResponse<Void> deleteGroup(@RequestHeader(name = "userId") Long userId,
-                                                                    @PathVariable(name = "groupId") Long groupId) {
+    public SuccessResponse<Void> deleteGroup(
+        @Auth final Accessor accessor,
+        @PathVariable(name = "groupId") Long groupId) {
 
-        groupService.deleteGroup(userId, groupId);
+        groupService.deleteGroup(accessor.getUserId(), groupId);
         return new SuccessResponse<>();
     }
 
-    @PostMapping("/{groupId}/join") // 여기 헤더로 그룹 id를 받아올 필요가 없음. 어차피 그룹코드로 그룹을 찾아오기 때문. 수정해야 함!!!!
+    @PostMapping("/join")
+    @UesrOnly
     @Operation(summary = "참여코드로 그룹에 참가하는 API", description = "참여 코드를 입력하여 그룹에 참가합니다.")
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201",description = "OK, 성공입니다.")
     })
     @Parameters({
         @Parameter(name = "groupId", description = "그룹의 아이디, path variable입니다."),
-        @Parameter(name = "userId", description = "유저의 아이디, header에 담아주시면 됩니다.")
     })
-    public SuccessResponse<GroupJoinResponse> joinGroup(@RequestHeader(name = "userId") Long userId,
-                                                                    @RequestBody @Valid GroupJoinRequest request) {
+    public SuccessResponse<GroupJoinResponse> joinGroup(
+        @Auth final Accessor accessor,
+        @RequestBody @Valid GroupJoinRequest request) {
 
-        UserGroup userGroup = groupService.joinGroup(userId, request);
+        UserGroup userGroup = groupService.joinGroup(accessor.getUserId(), request);
 
         return new SuccessResponse<>(GroupConverter.joinGroup(userGroup.getGroup(), userGroup.getUser()));
     }

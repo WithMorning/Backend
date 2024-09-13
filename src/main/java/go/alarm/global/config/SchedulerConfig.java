7419f9e644069111fe0ec0a075c0edffccd8330b
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-09-13 20:33:47 +0900

    refactor: 기상 알람 스케줄러 주석 제거

--- src/main/java/go/alarm/global/config/SchedulerConfig.java
+++ src/main/java/go/alarm/global/config/SchedulerConfig.java
@@ -3,6 +3,7 @@ package go.alarm.global.config;
 import go.alarm.fcm.service.FCMService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableScheduling
@@ -19,11 +20,9 @@ public class SchedulerConfig {
     * "0 0 9 * * MON-FRI": 평일 오전 9시에 실행
     * */
 
-
-    /*
     @Scheduled(cron = "0 * * * * *") // 매분 실행
     public void scheduleAlarms() {
         fcmService.sendAlarms();  // 테스트를 위해 스케줄러 실행 일시 정지
-    }*/
+    }
 
 }

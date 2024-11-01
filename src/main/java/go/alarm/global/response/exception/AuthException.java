dd86be0b59af046fcd79d3ad577ce19e7cd0a108
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-11-01 16:52:55 +0900

    refactor: 예외 상세 내용 출력하도록 수정

--- src/main/java/go/alarm/global/response/exception/AuthException.java
+++ src/main/java/go/alarm/global/response/exception/AuthException.java
@@ -10,7 +10,16 @@ public class AuthException extends RuntimeException {
     private final String message;
 
     public AuthException(ResponseCode responseCode) {
+        super(responseCode.getMessage());  // 부모 클래스에 메시지 전달
         this.code = responseCode.getCode();
         this.message = responseCode.getMessage();
     }
+
+    // cause를 받는 생성자 추가
+    public AuthException(ResponseCode responseCode, Throwable cause) {
+        super(responseCode.getMessage(), cause);  // 부모 클래스에 메시지와 cause 전달
+        this.code = responseCode.getCode();
+        this.message = responseCode.getMessage();
+    }
+
 }

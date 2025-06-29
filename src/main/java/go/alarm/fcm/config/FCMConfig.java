5d1b6b72aedc4e894219b9637814e250a07d5ca5
Muokok <sgo5178@kyonggi.ac.kr>
2025-06-29 15:00:14 +0900

    refactor: firebase key 경로 수정

--- src/main/java/go/alarm/fcm/config/FCMConfig.java
+++ src/main/java/go/alarm/fcm/config/FCMConfig.java
@@ -6,28 +6,34 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import java.io.IOException;
 import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 @Configuration
 public class FCMConfig {
+    @Value("${fcm.service-account-file}")
+    private String firebaseKeyPath;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        // 프로젝트 파일과 동일한 디렉토리에 있는 firebase_service_key.json 파일 경로
-        String filePath = "./firebase_service_key.json";
 
-        // FileSystemResource를 사용하여 파일 시스템에서 리소스를 로드
-        Resource resource = new FileSystemResource(filePath);
+        Resource resource = new ClassPathResource(firebaseKeyPath.replace("file:", " "));
         InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .build();
 
-        return FirebaseApp.initializeApp(options);
+        // 이미 초기화되었는지 확인하여 중복 초기화를 방지
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
     }
 
     @Bean
